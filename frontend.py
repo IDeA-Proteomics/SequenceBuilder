f7ac8b164d5fb61b8d58b21bbe6c82e3c476feaa
David Caldwell <dcaldwell3@uams.edu>
2024-08-02 14:50:20 -0500

    Add tray picker and start position

--- frontend.py
+++ frontend.py
@@ -1,6 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
 from IDeA_classes import SampleList
+import re
 
+### 1 = RED
+### 2 - GREEN
+### 3 - BLUE
+
+class SFE_TrayPicker(tk.Frame):
+
+    def __init__(self, parent, select_var, command):
+        self.parent = parent
+
+        tk.Frame.__init__(self, self.parent)
+
+        self.red_button = tk.Radiobutton(self, text="RED", var=select_var, value="RED", fg='black', bg='red', width = 4, command=command)
+        self.green_button = tk.Radiobutton(self, text="GREEN", var=select_var, value="GREEN", fg='black', bg='green', width = 4, command=command)
+        self.blue_button = tk.Radiobutton(self, text="BLUE", var=select_var, value="BLUE", fg = 'black', bg='blue', width = 4, command=command)
+
+        self.red_button.pack()
+        self.green_button.pack()
+        self.blue_button.pack()        
+
+        return
 
 class LabelAndText(tk.Frame):
 
@@ -16,6 +38,20 @@ class LabelAndText(tk.Frame):
         self.text.pack(side=tk.LEFT)
 
         return
+    
+class LabelAndCombo(tk.Frame):
+
+    def __init__(self, parent, label, var, choices):
+        self.parent = parent
+        tk.Frame.__init__(self, self.parent)
+
+        self.combo = ttk.Combobox(self.parent, textvariable=var, values=choices, state='readonly')
+        self.combo.pack()
+
+
+
+        return
+    
 
 class SFE_Head(tk.Frame):
 
@@ -81,21 +117,61 @@ class SequenceFrontEnd:
         # self.sample_list = sample_list
 
         self.parent = parent
+        self.sample_list = sample_list
+        self.tray_selection = tk.StringVar(value = "BLUE")
+        self.start_position = tk.StringVar(value = "BA1")
 
-        self.label = tk.Label(self.parent, text = "Hello World")
-        self.label.pack()
+        self.pos_choices = self.getPositionList()
 
-        self.head = SFE_Head(self.parent, sample_list)
+        self.buildUI()
+        return
+    
+    def getPositionList(self):
+        rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        retval = [self.tray_selection.get()[:1] + row + str(pos) for row in rows for pos in range(1,13)]
+        return retval
+
+
+    def handleTraySelection(self):
+        initial = self.tray_selection.get()[:1]
+        self.start_position.set(initial + self.start_position.get()[1:])
+        self.pos_choices = self.getPositionList()
+        self.start_well.combo.config(values = self.pos_choices)
+        return
+
+
+    def buildUI(self):
+
+        self.main_frame = tk.Frame(self.parent)
+        self.left_frame = tk.Frame(self.main_frame)
+        self.right_frame = tk.Frame(self.main_frame)
+        self.bottom_frame = tk.Frame(self.parent)
+
+        self.main_frame.pack(side = tk.TOP)
+        self.left_frame.pack(side = tk.LEFT)
+        self.right_frame.pack(side = tk.LEFT)
+        self.bottom_frame.pack(side = tk.TOP)
+
+
+        self.head = SFE_Head(self.left_frame, self.sample_list)
         self.head.pack(side = tk.TOP, anchor=tk.W)
 
-        self.list_frame = SFE_ListFrame(self.parent, sample_list)
+        self.list_frame = SFE_ListFrame(self.left_frame, self.sample_list)
         self.list_frame.pack()
 
-        self.exit_button = tk.Button(self.parent, text = "Exit", command = parent.destroy)
+        self.start_well = LabelAndCombo(self.right_frame, label = "Start Pos", var = self.start_position, choices = self.pos_choices)
+        self.start_well.pack()
+
+        self.tray_selector = SFE_TrayPicker(self.right_frame, self.tray_selection, self.handleTraySelection)
+        self.tray_selector.pack()
+
+        self.exit_button = tk.Button(self.bottom_frame, text = "Exit", command = self.parent.destroy)
         self.exit_button.pack()
 
+        return
 
-        
+
+
 
 def showSeqFE(sample_list):
     root = tk.Tk()
