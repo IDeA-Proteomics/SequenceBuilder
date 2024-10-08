4b9b725d415182e5cda61362f26fadd3906bdccf
David Caldwell <dcaldwell3@uams.edu>
2024-10-08 10:37:36 -0500

    Shortcut has to set cwd

--- frontend.py
+++ frontend.py
@@ -496,7 +496,7 @@ class SequenceFrontEnd:
 
 
 def showSeqFE():
-    os.chdir("C:\\Automate\\")
+    # os.chdir("C:\\Automate\\")
     root = tk.Tk()
     root.eval('tk::PlaceWindow . center')
     root.iconbitmap("IDEA_Logo.ico")
