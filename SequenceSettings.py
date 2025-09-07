035fe446bf57e569a85a34e29b1a8aea840cccc3
david <6957239+delta-G@users.noreply.github.com>
2025-09-06 19:56:52 -0500

    Save and load settings to json

--- SequenceSettings.py
+++ SequenceSettings.py
@@ -1,11 +1,27 @@
-
+import json
+import os
 
 
 class AppSettings():
 
     def __init__(self):
-        self.default_tray = 'G'
-        self.default_instrument = "Exploris1"
-        self.default_save_folder = "/home/david/IDeA_Scripts/TestData/"
-
+        if os.path.exists("settings.json"):
+            self.loadFromFile("settings.json")
+        else:
+            self.default_tray = 'G'
+            self.default_instrument = "Exploris1"
+            self.default_save_folder = "/home/david/IDeA_Scripts/TestData/"
+            self.saveToFile("settings.json")
         return
+    
+    def saveToFile(self, file_path):
+        with open(file_path, 'w') as f:
+            json.dump(self.__dict__, f, indent=4)
+        return
+    
+    def loadFromFile(self, file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            self.__dict__.update(data)
+        return
+    
