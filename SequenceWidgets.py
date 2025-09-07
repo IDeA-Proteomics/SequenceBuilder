62bc47517395e7ca5b6e1e5e09f287357553d5d3
david <6957239+delta-G@users.noreply.github.com>
2025-09-06 23:15:41 -0500

    Moved TrayPicker to SequenceWidgets package

--- SequenceWidgets.py
+++ SequenceWidgets.py
@@ -35,3 +35,36 @@ class PreAndPostCheck(tk.Frame):
         self.post.pack(side=tk.LEFT)
 
         return
+
+class TrayPicker(tk.Frame):
+
+    def __init__(self, parent, textvariable, onChange, initial_val):
+        self.parent = parent
+
+        tk.Frame.__init__(self, self.parent)
+        self.textvariable = textvariable
+        self.onChange = onChange
+
+        self.buttons = [
+            tk.Radiobutton(self, text="", var=self.textvariable, value="R", fg='black', bg='red', width = 4, command=self.onChange),
+            tk.Radiobutton(self, text="", var=self.textvariable, value="G", fg='black', bg='green', width = 4, command=self.onChange),
+            tk.Radiobutton(self, text="", var=self.textvariable, value="B", fg = 'black', bg='blue', width = 4, command=self.onChange),
+            tk.Radiobutton(self, text="", var=self.textvariable, value="Y", fg = 'black', bg='yellow', width = 4, command=self.onChange)
+        ]
+        for button in self.buttons:
+            button.pack(side = tk.LEFT)
+            if button.cget('value') == initial_val:
+                button.select()
+
+        return
+    
+    def refreshState(self, trays):
+        for button in self.buttons:
+            if button.cget('value') in trays:
+                button.config(state="normal")
+            else:
+                button.config(state="disabled")
+                if button.cget('value') == self.textvariable.get():
+                    self.textvariable.set(trays[1])
+                    self.onChange()
+        return
