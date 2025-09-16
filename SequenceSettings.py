248fd70d225a1ee047a5cb5e4a31343752d5828b
david <6957239+delta-G@users.noreply.github.com>
2025-09-15 21:35:46 -0500

    Fix default settings so it works on update

--- SequenceSettings.py
+++ SequenceSettings.py
@@ -11,6 +11,7 @@ class AppSettings():
             self.default_tray = 'G'
             self.default_instrument = "Exploris1"
             self.default_save_folder = "/home/david/IDeA_Scripts/TestData/"
+            self.default_vertical = False
             self.saveToFile("settings.json")
         return
     
@@ -22,6 +23,21 @@ class AppSettings():
     def loadFromFile(self, file_path="settings.json"):
         with open(file_path, 'r') as f:
             data = json.load(f)
+            missing = False
+            if 'default_tray' not in data:
+                data['default_tray'] = 'G'
+                missing = True
+            if 'default_instrument' not in data:
+                data['default_instrument'] = "Exploris1"
+                missing = True
+            if 'default_save_folder' not in data:
+                data['default_save_folder'] = ""
+                missing = True
+            if 'default_vertical' not in data:
+                data['default_vertical'] = False
+                missing = True
             self.__dict__.update(data)
+            if missing:
+                self.saveToFile(file_path)
         return
     
